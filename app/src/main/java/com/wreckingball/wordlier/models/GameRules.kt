a85b71388454798031a3831afe90f5577628530b
Lee Waggoner <lwaggoner@roadrunner.com>
2023-07-08 13:05:49 +0800

    Fixed letter checker

--- app/src/main/java/com/wreckingball/wordlier/models/GameRules.kt
+++ app/src/main/java/com/wreckingball/wordlier/models/GameRules.kt
@@ -6,23 +6,67 @@ import com.wreckingball.wordlier.ui.theme.WrongLetterCell
 import com.wreckingball.wordlier.ui.theme.WrongPositionCell
 
 class GameRules {
-    fun colorLetters(theWord: String, guess: String) : List<Pair<Char, Color>> {
+    fun colorLetters(word: String, guess: String) : List<Pair<Char, Color>> {
+        val characterCount = initCharacterCount(word)
         val guessList = guess.toList()
-        val resultList = mutableListOf<Color>()
+        val resultList = mutableListOf(
+            WrongLetterCell,
+            WrongLetterCell,
+            WrongLetterCell,
+            WrongLetterCell,
+            WrongLetterCell
+        )
+
+//        for ((index, letter) in guessList.withIndex()) {
+//            if (word.contains(letter) && (characterCount[letter] ?: 0) > 0) {
+//                if (letter == word[index]) {
+//                    resultList[index] = CorrectLetterCell
+//                } else {
+//                    resultList[index] = WrongPositionCell
+//                }
+//                //need to mark a subsequent hit as a wrong letter
+//                val count = (characterCount[letter] ?: 1) - 1
+//                characterCount[letter] = count
+//            }
+//        }
+
+        //need to do two passes because word = adult and guess = trust shows the first 't' as
+        //WrongPositionCell instead of WrongLetterCell and the last 't' as WrongLetterCell
         for ((index, letter) in guessList.withIndex()) {
-            resultList.add(
-                if (theWord.contains(letter)) {
-                    if (letter == theWord[index]) {
-                        CorrectLetterCell
-                    } else {
-                        WrongPositionCell
-                    }
-                } else {
-                    WrongLetterCell
-                }
-            )
+            if (letter == word[index]) {
+                resultList[index] = CorrectLetterCell
+                //need to mark a subsequent hit as a wrong letter
+                val count = (characterCount[letter] ?: 1) - 1
+                characterCount[letter] = count
+            }
         }
+        for ((index, letter) in guessList.withIndex()) {
+            if (word.contains(letter)
+                && resultList[index] != CorrectLetterCell
+                && (characterCount[letter] ?: 0) > 0) {
+                resultList[index] = WrongPositionCell
+                //need to mark a subsequent hit as a wrong letter
+                val count = (characterCount[letter] ?: 1) - 1
+                characterCount[letter] = count
+            }
+        }
+
         return guessList.zip(resultList)
+    }
+
+    private fun initCharacterCount(word: String) : MutableMap<Char, Int> {
+        val characterCount = mutableMapOf<Char, Int>()
+        for (letter in word) {
+            if (characterCount.contains(letter)) {
+                val count = characterCount[letter]
+                count?.let {
+                    characterCount[letter] = it + 1
+                }
+            } else {
+                characterCount[letter] = 1
+            }
+        }
+        return characterCount
     }
 
     fun handleGuess(word: String, guess: String, currentRow: Int) : GameResult {
